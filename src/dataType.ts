39070b8319981ca622a5bb64346f9d204fe2e330
Alex-Weise <Sannek65@gmail.com>
2022-09-03 17:48:59 +0300

    Типизировал объект продукта, добавил функцию по созданию

---
+++ src/dataType.ts
@@ -0,0 +1,13 @@
+export type OneProduct = {
+    data: string,
+    type: string,
+    offtype: string,
+    category: string,
+    title: string,
+    text: string,
+    price: number | string,
+    discount: number | boolean,
+    ishot: boolean,
+    isnew: boolean,
+    images: string[],
+}
--- src/store/products.ts
+++ src/store/products.ts
@@ -1,129 +1,156 @@
-export const bookmark = [
+import { OneProduct } from "../dataType";
+
+const addNEW = (arr: OneProduct[], data: string, type: string, offtype:string, category:string, title:string, text:string, price:number, discount:boolean | number, ishot:boolean, isnew: boolean, images:number ) => {
+    let imageArr = [];
+    for (let i = 1; i <= images; i++) {
+        if (offtype) {
+           imageArr.push(`../../assets/data/${data}/${type}/${offtype}/${i}.jpg`);
+        } else {imageArr.push(`../../assets/data/${data}/${type}/${i}.jpg`)}
+    };
+    let obj = { // это единица продукта
+        //ниже указываем папки к картинкам после assets/data/
+        data: data, // указываем папку (категории на англ)
+        type: type, // указываем внутреннюю папку (подкатегории или же сама папка товара)
+        offtype: offtype, // пустая строка, или же папка товара
+        //ниже уже о товаре
+        category: category, // категории товаров "Открытка", "Брелок" и тд
+        title: title, // название товара
+        text: text, // все описание товара (разделять точкой (.))
+        price: price, // цена, без Р
+        discount: discount, // цена по скидке, на будущее если нужно =)
+        ishot: ishot, // популярный товар, да или нет
+        isnew: isnew, // новинка, да или нет
+        images: imageArr, // тут уже картинки, пусть будет 1 -ая картинка основная, а остальные листать можно
+        // в функции по созданию нужно указать количество картинок в папке
+    };
+    arr.push(obj);
+};
+
+export const bookmark:OneProduct[] = [
     {
-        id: 1,
         data: "bookmark",
         type: "black",
+        offtype: "",
         category: "закладка",
         title: "черная закладка",
         text: "пластиковая односторонняя закладка для книг",
         price: 300,
         discount: false,
-        hot: false,
-        new: false,
+        ishot: false,
+        isnew: false,
         images: ["../../assets/data/bookmark/black/1.jpg"],
     },
     {
-        id: 2,
         data: "bookmark",
         type: "blue",
+        offtype: "",
         category: "закладки",
         title: "синяя закладка",
         text: "пластиковая односторонняя закладка для книг",
         price: 300,
         discount: false,
-        hot: false,
-        new: false,
+        ishot: false,
+        isnew: false,
         images: ["../../assets/data/bookmark/blue/1.jpg"],
     },
 ];
-export const pin = [
+export const pin:OneProduct[] = [
     {
-        id: 3,
         data: "pin",
         type: "pin1",
+        offtype: "",
         category: "значки",
         title: "пин",
         text: "пин из эпоксидной смолы с жёлтой застёжкой-сердечком",
         price: 250,
         discount: false,
-        hot: false,
-        new: false,
+        ishot: false,
+        isnew: false,
         images: ["../../assets/data/pin/pin1/1.jpg","../../assets/data/pin/pin1/2.jpg"],
     },
 ];
-export const postcard = [
+export const postcard:OneProduct[] = [
     {
-        id: 4,
         data: "postcard",
         type: "date",
+        offtype: "",
         category: "открытки",
         title: "свидание",
         text: "односторонняя печать. текстурная бумага плотностю 250 гр. формат А6",
         price: 100,
         discount: false,
-        hot: false,
-        new: false,
+        ishot: false,
+        isnew: false,
         images: ["../../assets/data/postcard/date/1.jpg"],
     },
     {
-        id: 5,
         data: "postcard",
         type: "dont",
+        offtype: "",
         category: "открытки",
         title: "не потеряй себя",
         text: "односторонняя печать. текстурная бумага плотностю 250 гр. формат А6",
         price: 100,
         discount: false,
-        hot: false,
-        new: false,
+        ishot: false,
+        isnew: false,
         images: ["../../assets/data/postcard/dont/1.jpg"],
     },
     {
-        id: 6,
         data: "postcard",
         type: "flowers",
+        offtype: "",
         category: "открытки",
         title: "цветы",
         text: "двусторонняя печать, на обороте место для тёплых слов. текстурная бумага плотностю 250 гр. формат А6",
         price: 100,
         discount: false,
-        hot: false,
-        new: false,
+        ishot: false,
+        isnew: false,
         images: ["../../assets/data/postcard/flowers/1.jpg"],
     },
     {
-        id: 7,
         data: "postcard",
         type: "june",
+        offtype: "",
         category: "открытки",
         title: "июнь",
         text: "односторонняя печать. текстурная бумага плотностю 250 гр. формат А6",
         price: 100,
         discount: false,
-        hot: false,
-        new: false,
+        ishot: false,
+        isnew: false,
         images: ["../../assets/data/postcard/june/1.jpg"],
     },
     {
-        id: 8,
         data: "postcard",
         type: "mind",
+        offtype: "",
         category: "открытки",
         title: "мысли",
         text: "односторонняя печать. текстурная бумага плотностю 250 гр. формат А6",
         price: 100,
         discount: false,
-        hot: false,
-        new: false,
+        ishot: false,
+        isnew: false,
         images: ["../../assets/data/postcard/mind/1.jpg"],
     },
     {
-        id: 9,
         data: "postcard",
         type: "ticket",
+        offtype: "",
         category: "открытки",
         title: "билет в один конец",
         text: "односторонняя печать. текстурная бумага плотностю 250 гр. формат А6",
         price: 100,
         discount: false,
-        hot: false,
-        new: false,
+        ishot: false,
+        isnew: false,
         images: ["../../assets/data/postcard/ticket/1.jpg"],
     },
 ];
-export const stickers = [
+export const stickers:OneProduct[] = [
     {
-        id: 10,
         data: "stickers",
         type: "a6",
         offtype: "flowers",
@@ -132,12 +159,11 @@ export const stickers = [
         text: "материал: виниловая плёнка с матовой ламинацией. вырубка по форме каждого стикера. формат А6",
         price: 130,
         discount: false,
-        hot: false,
-        new: false,
+        ishot: false,
+        isnew: false,
         images: ["../../assets/data/stickers/a6/flowers/1.jpg"],
     },
     {
-        id: 11,
         data: "stickers",
         type: "a6",
         offtype: "neer",
@@ -146,12 +172,11 @@ export const stickers = [
         text: "материал: виниловая плёнка с матовой ламинацией. вырубка по форме каждого стикера. формат А6",
         price: 130,
         discount: false,
-        hot: false,
-        new: false,
+        ishot: false,
+        isnew: false,
         images: ["../../assets/data/stickers/a6/neer/1.jpg"],
     },
     {
-        id: 12,
         data: "stickers",
         type: "a6",
         offtype: "ocean",
@@ -160,12 +185,11 @@ export const stickers = [
         text: "материал: виниловая плёнка с матовой ламинацией. вырубка по форме каждого стикера. формат А6",
         price: 130,
         discount: false,
-        hot: false,
-        new: false,
+        ishot: false,
+        isnew: false,
         images: ["../../assets/data/stickers/a6/ocean/1.jpg"],
     },
     {
-        id: 13,
         data: "stickers",
         type: "a6",
         offtype: "sleep",
@@ -174,12 +198,11 @@ export const stickers = [
         text: "материал: виниловая плёнка с матовой ламинацией. вырубка по форме каждого стикера. формат А6",
         price: 130,
         discount: false,
-        hot: false,
-        new: false,
+        ishot: false,
+        isnew: false,
         images: ["../../assets/data/stickers/a6/sleep/1.jpg", "../../assets/data/stickers/a6/sleep/2.jpg"],
     },
     {
-        id: 14,
         data: "stickers",
         type: "a6",
         offtype: "summer",
@@ -188,12 +211,11 @@ export const stickers = [
         text: "материал: виниловая плёнка с матовой ламинацией. вырубка по форме каждого стикера. формат А6",
         price: 130,
         discount: false,
-        hot: false,
-        new: false,
+        ishot: false,
+        isnew: false,
         images: ["../../assets/data/stickers/a6/summer/1.jpg", "../../assets/data/stickers/a6/summer/2.jpg", "../../assets/data/stickers/a6/summer/3.jpg"],
     },
     {
-        id: 15,
         data: "stickers",
         type: "little",
         offtype: "copybook",
@@ -202,12 +224,11 @@ export const stickers = [
         text: "стикеры по книге Микиты Франко 'тетрадь в клеточку'. материал: виниловая плёнка с матовой ламинацией. вырубка по форме каждого стикера. размер 10*10см",
         price: 110,
         discount: false,
-        hot: false,
-        new: false,
+        ishot: false,
+        isnew: false,
         images: ["../../assets/data/stickers/little/copybook/1.jpg"],
     },
     {
-        id: 16,
         data: "stickers",
         type: "little",
         offtype: "life",
@@ -216,12 +237,11 @@ export const stickers = [
         text: "стикеры по книге Микиты Франко 'дни нашей жизни'. материал: виниловая плёнка с матовой ламинацией. вырубка по форме каждого стикера. размер 10*10см",
         price: 110,
         discount: false,
-        hot: false,
-        new: false,
+        ishot: false,
+        isnew: false,
         images: ["../../assets/data/stickers/little/life/1.jpg"],
     },
     {
-        id: 17,
         data: "stickers",
         type: "little",
         offtype: "mini",
@@ -230,12 +250,11 @@ export const stickers = [
         text: "материал: виниловая плёнка с матовой ламинацией. размер 5*5см",
         price: 30,
         discount: false,
-        hot: false,
-        new: false,
+        ishot: false,
+        isnew: false,
         images: ["../../assets/data/stickers/little/mini/1.jpg"],
     },
     {
-        id: 18,
         data: "stickers",
         type: "little",
         offtype: "soul",
@@ -244,23 +263,25 @@ export const stickers = [
         text: "материал: виниловая плёнка с матовой ламинацией. вырубка по форме каждого стикера. размер 7*7см",
         price: 50,
         discount: false,
-        hot: false,
-        new: false,
+        ishot: false,
+        isnew: false,
         images: ["../../assets/data/stickers/little/soul/1.jpg"],
     },
 ];
-export const trinket = [
+export const trinket:OneProduct[] = [
     {
-        id: 19,
         data: "trinket",
         type: "trinket1",
+        offtype: "",
         category: "брелки",
         title: "брелок",
         text: "акриловый брелок с карабином-звёздочкой",
         price: 350,
         discount: false,
-        hot: false,
-        new: false,
+        ishot: false,
+        isnew: false,
         images: ["../../assets/data/trinket/trinket1/1.jpg"],
     },
-]
+];
+
+export const allProd:OneProduct[] = [...bookmark, ...pin, ...postcard, ...stickers, ...trinket];
